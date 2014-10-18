f9cd3e93be5396b4201dcb277f27cfae84c49454
Jamie Davies <viralpickaxe@gmail.com>
2014-10-18 23:56:21 +0100

    Added fake endpoint for schedule

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var migration_landmarks = require('./migrations/migration-landmarks')
 
 var server = restify.createServer();
 
+server.use(restify.queryParser({mapParams:true}));
+
 server.get('/json', function(req, res, next) {
   res.send({json: 'test', yolo: 'swagger'})
   next()
@@ -26,6 +28,50 @@ server.get('/landmarks/migrate', function(req, res, next) {
   Landmark.find({}).remove().exec();
   migration_landmarks.migrate()
   res.send({success:true})
+  next()
+})
+
+server.get('/schedule', function(req, res, next) {
+  var ids = req.params.ids.split(',')
+
+  // Do some fancy alex stuff here
+
+  var sample = [
+    {
+      landmark: {
+        name: "Big Ben",
+        image_url: "http://images.rapgenius.com/du3efo4t81u169zb6516x29ev.1000x375x1.jpg",
+        coords: [0,0]
+      },
+      timeframe: {
+        start: 131231232,
+        end: 3123123123
+      }
+    },
+    {
+      landmark: {
+        name: "Big Ben",
+        image_url: "http://images.rapgenius.com/du3efo4t81u169zb6516x29ev.1000x375x1.jpg",
+        coords: [0,0]
+      },
+      timeframe: {
+        start: 131231232,
+        end: 3123123123
+      }
+    },
+    {
+      landmark: {
+        name: "Big Ben",
+        image_url: "http://images.rapgenius.com/du3efo4t81u169zb6516x29ev.1000x375x1.jpg",
+        coords: [0,0]
+      },
+      timeframe: {
+        start: 131231232,
+        end: 3123123123
+      }
+    }
+  ];
+  res.send(sample)
   next()
 })
 
