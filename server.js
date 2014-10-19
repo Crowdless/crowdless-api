fea67126cc5593f6ee21a2081633cafa768faa86
Alexander Christie <alexanderchristie@outlook.com>
2014-10-19 03:46:27 +0100

    Remove app crap

--- server.js
+++ server.js
@@ -115,64 +115,6 @@ server.get('/schedule', function(req, res, next) {
   // })
 })
 
-server.get('/redo', function(req, res, next) {
-  Activity.find({}).limit(300).exec(function (err_activity, results_activity) {
-    Landmark.find({}).exec(function (err_landmark, results_landmark) {
-      var cluster = new require('./math/clustering').clusterer(results_activity,results_landmark)
-    })
-  })
-})
-
-server.get('/calc', function(req, res, next) {
-  Landmark.find({}).exec(function(err, records_landmarks){
-    Activity.find({}).limit(2000).exec(function(err, records_activities){
-      for(var landmark_n in records_landmarks){
-        var landmark = records_landmarks[landmark_n]
-        var landmark_counters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        for(var activity_n in records_activities){
-          var activity = records_activities[activity_n]
-          var distance = geolib.getDistance(
-            { latitude: landmark.coords[0], longitude: landmark.coords[1] },
-            { latitude: activity.interaction.interaction.geo.latitude, longitude: activity.interaction.interaction.geo.longitude }
-          )
-
-          if(distance<=1000){
-            var time = moment(activity.interaction.interaction.created_at)
-            var hour = time.hour()
-            landmark_counters[hour]++;
-          }
-        }
-        var highest_index = 0
-        var highest_num = 0
-        for (var i = 0; i < landmark_counters.length; i++) {
-          var counter = landmark_counters[i]
-          if(counter>highest_num){
-            highest_index = i
-            highest_num = counter
-          }
-        };
-        var relative_count = []
-        for (var i = 0; i < landmark_counters.length; i++) {
-          relative_count[i] = landmark_counters[i]/highest_num
-        };
-        var net = new brain.NeuralNetwork()
-        var stupid = []
-        for (var i = 0; i < 24; i++) {
-          stupid.push({input: {h:i/24}, output: {busy:relative_count[i]}});
-        };
-        net.train(stupid)
-        console.log(net.run({h:2/24}))
-        console.log(net.run({h:6/24}))
-      }
-    })
-  })
-})
-
-server.get('/cluster', function(req, res, next) {
-    res.send()
-    next()
-})
-
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
